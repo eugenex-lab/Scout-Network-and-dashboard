a60b1c18ed2f43c7f97754a6934a37c687478104
eugenex-lab <kolapo.eugene@gmail.com>
2022-08-02 14:32:17 +0100

    clean slit

---
+++ src/components/ScoutDetails/LIstPlayer.vue
@@ -0,0 +1,21 @@
+<template>
+  <base-card>
+    <li>
+      {{ item }}
+      <button v-on:click="$emit('remove')">Remove</button>
+    </li>
+  </base-card>
+</template>
+
+<script>
+import BaseCard from "@/components/UI/BaseCard";
+export default {
+  name: "Remove",
+  components: {BaseCard},
+  props:['item'],
+}
+</script>
+
+<style scoped>
+
+</style>
